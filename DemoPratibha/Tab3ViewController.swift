0d7cf1550354b8d2d8aae7d30e72e7f9d10e45ac
vivek <vivek.g@intelegain.com>
2019-05-18 12:29:12 +0530

    Initial Commit

---
+++ DemoPratibha/Tab3ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  Tab3ViewController.swift
+//  DemoPratibha
+//
+//  Created by Vivek Gajbe on 5/17/19.
+//  Copyright © 2019 Intelegain. All rights reserved.
+//
+
+import UIKit
+
+class Tab3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnNextClicked(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
